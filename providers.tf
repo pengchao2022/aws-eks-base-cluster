07529a1e0bb7caf0d5c31b8da57f16531d764264
pengchao <pengchao.ma2@outlook.com>
2025-09-09 15:53:46 +0800

    udpated

--- providers.tf
+++ providers.tf
@@ -1,8 +1,14 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_version = ">= 1.0"
 
-
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.95.0, < 6.0.0" # 满足 EKS 20.0 的要求
+    }
+  }
 }
 
-
-provider "random" {}
+provider "aws" {
+  region = "us-east-1" # 根据你的需求修改区域
+}
