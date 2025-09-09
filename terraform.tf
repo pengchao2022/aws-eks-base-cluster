f083a35b589ae199ae989f31ee1413343c7c78df
pengchao <pengchao.ma2@outlook.com>
2025-09-09 15:15:26 +0800

    modified

--- terraform.tf
+++ terraform.tf
@@ -2,10 +2,7 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0" # 降级到兼容版本
-    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
