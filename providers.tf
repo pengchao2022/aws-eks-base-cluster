9dec36980979a6356420fb80b031fe33205d5e8a
pengchao <pengchao.ma2@outlook.com>
2025-09-09 16:05:01 +0800

    modified

--- providers.tf
+++ providers.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.95.0, < 6.0.0" # 满足 EKS 20.0 的要求
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-east-1" # 根据你的需求修改区域
+  region = "us-east-1"  # 根据您的需求修改区域
 }
