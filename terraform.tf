7b7710b6fd38eefdb15f8dd02e394b745562dc27
pengchao <pengchao.ma2@outlook.com>
2025-09-09 15:58:53 +0800

    updaated

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,10 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # 添加 AWS provider，使用 5.x 版本以兼容 EKS 模块
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
