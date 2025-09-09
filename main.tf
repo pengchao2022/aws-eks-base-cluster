685b3a9457323e612e541b4992e4323b6d96011e
pengchao <pengchao.ma2@outlook.com>
2025-09-09 15:18:49 +0800

    updated

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.11"
-    }
-  }
-}
-
 data "aws_caller_identity" "current" {}
 
 module "eks" {
--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,10 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0" # 降级到兼容版本
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
