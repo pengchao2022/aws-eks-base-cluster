2ca3bfb8f2baeb524e4567e31b6b5f29014bae95
pengchao <pengchao.ma2@outlook.com>
2025-09-13 19:10:12 +0800

    hello

--- main.tf
+++ main.tf
@@ -85,12 +85,24 @@ resource "aws_iam_role_policy_attachment" "nodes_AmazonSSMManagedInstanceCore" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.nodes.name
 }
+
+# 安全组 for EKS 节点
+resource "aws_security_group" "eks_nodes" {
+  name_prefix = "${var.cluster_name}-nodes-"
+  vpc_id      = var.vpc_id
+  description = "Security group for EKS worker nodes"
+
+  tags = {
+    Name = "${var.cluster_name}-nodes-sg"
+  }
+}
+
 # 获取集群的安全组ID
 data "aws_eks_cluster" "this" {
   name = var.cluster_name
 }
 
-# 允许节点安全组访问集群的443端口（API Server）
+# 安全组规则 - 允许节点访问集群
 resource "aws_security_group_rule" "nodes_to_cluster_api" {
   description              = "Allow nodes to communicate with cluster API server"
   from_port                = 443
@@ -101,28 +113,6 @@ resource "aws_security_group_rule" "nodes_to_cluster_api" {
   type                     = "ingress"
 }
 
-# 允许节点安全组访问集群的DNS端口（如果使用CoreDNS）
-resource "aws_security_group_rule" "nodes_to_cluster_dns" {
-  description              = "Allow nodes to communicate with cluster DNS"
-  from_port                = 53
-  to_port                  = 53
-  protocol                 = "tcp"
-  security_group_id        = data.aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
-  source_security_group_id = aws_security_group.eks_nodes.id
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "nodes_to_cluster_dns_udp" {
-  description              = "Allow nodes to communicate with cluster DNS (UDP)"
-  from_port                = 53
-  to_port                  = 53
-  protocol                 = "udp"
-  security_group_id        = data.aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
-  source_security_group_id = aws_security_group.eks_nodes.id
-  type                     = "ingress"
-}
-
-# 允许集群安全组访问节点的kubelet端口（1025-65535）
 resource "aws_security_group_rule" "cluster_to_nodes_kubelet" {
   description              = "Allow cluster to communicate with nodes kubelet"
   from_port                = 1025
@@ -133,7 +123,6 @@ resource "aws_security_group_rule" "cluster_to_nodes_kubelet" {
   type                     = "ingress"
 }
 
-# 允许节点间通信的所有端口
 resource "aws_security_group_rule" "nodes_to_nodes" {
   description              = "Allow nodes to communicate with each other"
   from_port                = 0
@@ -144,7 +133,6 @@ resource "aws_security_group_rule" "nodes_to_nodes" {
   type                     = "ingress"
 }
 
-# 允许节点访问互联网（出站）
 resource "aws_security_group_rule" "nodes_to_internet" {
   description       = "Allow nodes to access internet"
   from_port         = 0
@@ -155,31 +143,26 @@ resource "aws_security_group_rule" "nodes_to_internet" {
   type              = "egress"
 }
 
-
-
-
-
-
-# EKS Node Group - 使用最简单的配置
+# EKS Node Group - 简化配置
 resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "main-nodes"
+  node_group_name = "simple-nodes"
   node_role_arn   = aws_iam_role.nodes.arn
   subnet_ids      = var.private_subnets
 
   scaling_config {
-    desired_size = 4
-    min_size     = 4
-    max_size     = 4
+    desired_size = 2 # 先创建2个节点
+    min_size     = 2
+    max_size     = 2
   }
 
+  # 最简配置
   ami_type       = "AL2_x86_64"
   instance_types = ["t3.micro"]
-  capacity_type  = "ON_DEMAND"
   disk_size      = 20
 
   tags = {
-    Name = "${var.cluster_name}-nodes"
+    Name = "simple-nodes"
   }
 
   depends_on = [
@@ -187,9 +170,9 @@ resource "aws_eks_node_group" "nodes" {
     aws_iam_role_policy_attachment.nodes_AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.nodes_AmazonEC2ContainerRegistryReadOnly,
     aws_iam_role_policy_attachment.nodes_AmazonSSMManagedInstanceCore,
-    aws_eks_cluster.this,
-    aws_security_group.eks_nodes,
-    aws_security_group_rule.cluster_to_nodes,
+    aws_security_group_rule.nodes_to_cluster_api,
+    aws_security_group_rule.cluster_to_nodes_kubelet,
     aws_security_group_rule.nodes_to_nodes,
+    aws_security_group_rule.nodes_to_internet,
   ]
 }
--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,11 @@ output "node_group_status" {
   value       = aws_eks_node_group.nodes.status
 }
 
+output "node_security_group_id" {
+  description = "Node security group ID"
+  value       = aws_security_group.eks_nodes.id
+}
+
 output "config_command" {
   description = "Command to configure kubectl"
   value       = "aws eks update-kubeconfig --name ${var.cluster_name} --region ${var.region}"
