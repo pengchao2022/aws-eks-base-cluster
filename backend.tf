281aaaee159e25ba8371c8a1094c04775ca9ce8e
pengchao <pengchao.ma2@outlook.com>
2025-09-14 11:17:58 +0800

    new modified

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraformstatefile090909"
-    key            = "aws_eks_a_dev_terraform.tfstate"
+    key            = "aws_eks_karpenter_dev_terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
